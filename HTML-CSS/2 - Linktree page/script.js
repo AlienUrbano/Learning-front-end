57c8e7ab1cc27507b5c02ebcdc563c3d45300a91
Paulo Gabriel Neves Santos <Paulogabrielneves@hotmail.com>
2023-07-29 11:20:10 -0300

    update

--- HTML-CSS/2 - Linktree page/script.js
+++ HTML-CSS/2 - Linktree page/script.js
@@ -12,48 +12,46 @@ function toggleMode() {
   }
 }
 
-$(document).ready(function() {
-	// Inicialize o Slick Carousel para a classe .tech-icons
-	$('.tech-icons').slick({
-	  centerMode: true, // O ícone central será ampliado
-	  centerPadding: '0', // Não adicionar padding aos ícones centrais
-	  slidesToShow: 3, // Quantidade de ícones visíveis por vez
-	  speed: 600, // Velocidade da animação em milissegundos
-	  autoplay: true, // Inicia o carrossel automaticamente
-	  autoplaySpeed: 1000, // Intervalo entre as transições em milissegundos
-	  infinite: true,
-	  arrows:false,
-	  responsive: [
-		{
-		  breakpoint: 768,
-		  settings: {
-			slidesToShow: 3 // Quantidade de ícones visíveis em telas menores
-		  }
-		},
-		{
-		  breakpoint: 480,
-		  settings: {
-			slidesToShow: 3 // Quantidade de ícones visíveis em telas ainda menores
-		  }
-		}
-	  ]
-	});
+$(document).ready(function () {
+  $(".tech-icons").slick({
+    centerMode: true,
+    centerPadding: "0",
+    slidesToShow: 3,
+    speed: 600,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    infinite: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+    ],
   });
+});
 
-  function isMobileDevice() {
-	return window.innerWidth < 769;
+function isMobileDevice() {
+  return window.innerWidth < 769;
+}
+
+function setBodyBackground() {
+  const body = document.querySelector("body");
+  if (isMobileDevice()) {
+    body.style.background = "var(--bg-url) no-repeat top center/cover";
+  } else {
+    body.style.background =
+      "var(--bg-url-desktop) no-repeat center center/cover";
   }
-  
-  // Função para definir o background com base no tipo de dispositivo
-  function setBodyBackground() {
-	const body = document.querySelector('body');
-	if (isMobileDevice()) {
-	  body.style.background = 'var(--bg-url) no-repeat top center/cover';
-	} else {
-	  body.style.background = 'var(--bg-url-desktop) no-repeat center center/cover';
-	}
-  }
-  
-  // Chamar a função quando a página carregar e quando a janela for redimensionada
-  document.addEventListener('DOMContentLoaded', setBodyBackground);
-  window.addEventListener('resize', setBodyBackground);
+}
+
+document.addEventListener("DOMContentLoaded", setBodyBackground);
+window.addEventListener("resize", setBodyBackground);
